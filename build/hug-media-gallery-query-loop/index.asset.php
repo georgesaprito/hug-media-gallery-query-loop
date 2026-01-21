5f144e63ded9a3d7e5ccb4844f02fa430d2c5e85
George Saprito <george@hugconstruction.com>
2026-01-21 07:42:52 -0800

    Workflow: untrack the build folder to prevent merge conflicts

--- build/hug-media-gallery-query-loop/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => 'a7f3f356626b6214f26b');
--- build/hug-media-gallery-query-loop/view.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => 'b1deb4e868008a31acbb');
